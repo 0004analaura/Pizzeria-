3628d134c4ecf8c8b3b2416a01b032de14109584
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2024-08-03 11:15:20 -0600

    modificacion

--- Pizzeria/src/Main.java
+++ Pizzeria/src/Main.java
@@ -1,15 +1,38 @@
+import gt.edu.miumg.pizzeria.pizza.Pizza;
+import gt.edu.miumg.pizzeria.pizza.PizzaChicago;
 import gt.edu.miumg.pizzeria.sucursal.Empleado;
+import gt.edu.miumg.pizzeria.sucursal.Sucursal;
 
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Empleado empleado = new Empleado("Ana Laura", "repartidor", 4500);
+        Sucursal sucursal = new Sucursal();
+        sucursal.agregarEmpleado(empleado);
+        int opcion;
+        do {
+            System.out.println("Pizzeria Ana Lauris");
+            System.out.println("Ordenar una pizza: 1");
+            System.out.println("Selecciona una opcion: ");
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    PizzaChicago pizzaChicago = new PizzaChicago("pizza chicago", 18, 800.00, "cuadarada");
+                    empleado.tomarOrden();
+                    empleado.trabajar();
+            }
+        } while (opcion != -1);
 
-        Empleado empleado = new Empleado(nombre: "Juan", puesto: "Repartidor", salario: 1500 );
+
+    }
+
+
 
 
 
 
         }
-    }
-}
+
