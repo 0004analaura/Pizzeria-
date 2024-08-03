aa5336ca537504dcd1023eb2bb9d584e737d5ab3
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2024-08-03 10:53:45 -0600

    Parcial 1 de pizzeria

---
+++ Pizzeria/src/Main.java
@@ -0,0 +1,15 @@
+import gt.edu.miumg.pizzeria.sucursal.Empleado;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Empleado empleado = new Empleado(nombre: "Juan", puesto: "Repartidor", salario: 1500 );
+
+
+
+
+        }
+    }
+}
---
+++ Pizzeria/src/gt/edu/miumg/pizzeria/ingredientes/Ingrediente.java
@@ -0,0 +1,8 @@
+package gt.edu.miumg.pizzeria.ingredientes;
+
+public interface Ingrediente {
+    public String getNombre();
+
+    public int getCantidad();
+
+}
